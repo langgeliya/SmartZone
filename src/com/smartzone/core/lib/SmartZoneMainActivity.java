7ba8fdfd63eccc29f2667c02478927118434045d
majian <majian714@sina.com>
2014-10-23 18:31:19 +0800

    http

--- src/com/smartzone/core/lib/SmartZoneMainActivity.java
+++ src/com/smartzone/core/lib/SmartZoneMainActivity.java
@@ -2,11 +2,12 @@ package com.smartzone.core.lib;
 
 import java.util.ArrayList;
 
+import org.apache.http.Header;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.TranslateAnimation;
@@ -14,6 +15,8 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.smartzone.core.BaseFragmentActivity;
 import com.smartzone.core.R;
 import com.smartzone.core.adapter.MainViewPagerAdapter;
@@ -181,6 +184,32 @@ public class SmartZoneMainActivity extends BaseFragmentActivity {
 	protected void onStop() {
 		// TODO Auto-generated method stub
 		super.onStop();
+	}
+	
+	private void initNetWorking() {
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get("http://www.google.com", new AsyncHttpResponseHandler() {
+
+		    @Override
+		    public void onStart() {
+		        // called before request is started
+		    }
+
+		    @Override
+		    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+		        // called when response HTTP status is "200 OK"
+		    }
+
+		    @Override
+		    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+		        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+		    }
+
+		    @Override
+		    public void onRetry(int retryNo) {
+		        // called when request is retried
+			}
+		});
 	}
 
 }
