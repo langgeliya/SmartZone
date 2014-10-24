01648f426593680f0cf464c92cb8e2b987bfc9a7
majian <majian714@sina.com>
2014-10-24 12:39:07 +0800

    change lib

--- src/com/smartzone/core/lib/FirstPageFragment.java
+++ src/com/smartzone/core/lib/FirstPageFragment.java
@@ -1,5 +1,13 @@
 package com.smartzone.core.lib;
 
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.Header;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.smartzone.core.utils.LogUtils;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -22,6 +30,8 @@ public class FirstPageFragment extends Fragment {
 //		View mView = inflater.inflate(R.layout.play_list_frament, null);
 		TextView tv = new TextView(getActivity());
 		tv.setText("first page");
+		LogUtils.printByTag(LogUtils.TAG1, "oncreate");
+		initNetWorking();
 		return tv;
 	}
 
@@ -65,6 +75,44 @@ public class FirstPageFragment extends Fragment {
 	public void onStop() {
 		// TODO Auto-generated method stub
 		super.onStop();
+	}
+	
+	private void initNetWorking() {
+		String url = "http://58.68.225.142/sqtest/jsontest.php?a=list";
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get(url, new AsyncHttpResponseHandler() {
+
+		    @Override
+		    public void onStart() {
+		        // called before request is started
+		    	LogUtils.printByTag(LogUtils.TAG1, "start...");
+		    }
+
+		    @Override
+		    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+		        // called when response HTTP status is "200 OK"
+		    	LogUtils.printByTag(LogUtils.TAG1, "statusCode:" + statusCode);
+		    	if(statusCode == 200){
+		    		try {
+						LogUtils.printByTag(LogUtils.TAG1, new String(response, "gbk"));
+					} catch (UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		    	}
+		    }
+
+		    @Override
+		    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+		        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+		    	LogUtils.printByTag(LogUtils.TAG1, "statusCode:" + statusCode);
+		    }
+
+		    @Override
+		    public void onRetry(int retryNo) {
+		        // called when request is retried
+			}
+		});
 	}
 
 }
